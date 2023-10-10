c8f1839a974b29b1f22907ef86682cf35eccdb70
thiago <thenrique2012@gmail.com>
2023-10-10 17:43:26 -0300

    refactor: define UserModel getters and setter using Lombok

--- src/main/java/br/com/thenriquedb/todolist/user/UserController.java
+++ src/main/java/br/com/thenriquedb/todolist/user/UserController.java
@@ -12,8 +12,8 @@ public class UserController {
 
     @PostMapping
     public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);
-        System.out.println(userModel.username);
-        System.out.println(userModel.password);
+        System.out.println(userModel.getName());
+        System.out.println(userModel.getUsername());
+        System.out.println(userModel.getPassword());
     }
 }

903fbf60758914630bddc089cecc8acd69aecb55
thiago <thenrique2012@gmail.com>
2023-10-10 19:03:01 -0300

    feat: create new User method in UserController

--- src/main/java/br/com/thenriquedb/todolist/user/UserController.java
+++ src/main/java/br/com/thenriquedb/todolist/user/UserController.java
@@ -1,19 +1,31 @@
 package br.com.thenriquedb.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private IUserRepository userRepository;
+
     @GetMapping
     public  String get() {
         return "Any user";
     }
 
     @PostMapping
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getName());
-        System.out.println(userModel.getUsername());
-        System.out.println(userModel.getPassword());
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("User already exists");
+        }
+
+        var createdUser =  this.userRepository.save(userModel);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
     }
 }

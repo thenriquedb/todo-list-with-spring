b54adac85ad057cf2ac82234bfd7fd00d0a52cb4
thiago <thenrique2012@gmail.com>
2023-10-11 18:43:28 -0300

    feat: create TaskController

---
+++ src/main/java/br/com/thenriquedb/todolist/task/TaskController.java
@@ -0,0 +1,30 @@
+package br.com.thenriquedb.todolist.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/task")
+public class TaskController {
+    @Autowired
+    private ITaskRepository taskRepository;
+
+    @PostMapping
+    public ResponseEntity create(@RequestBody TaskModel taskModel) {
+        var task = this.taskRepository.findByTitle(taskModel.getTitle());
+
+        if(task != null) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("There is already a task with this title");
+        }
+
+
+        var createdTask = this.taskRepository.save(taskModel);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdTask);
+    }
+}

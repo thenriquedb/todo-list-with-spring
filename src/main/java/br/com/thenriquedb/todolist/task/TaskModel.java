6c157cbfac59d12da47eb430e3046fffd5bab580
thiago <thenrique2012@gmail.com>
2023-10-11 18:09:33 -0300

    chore: create TaskModel

---
+++ src/main/java/br/com/thenriquedb/todolist/task/TaskModel.java
@@ -0,0 +1,35 @@
+package br.com.thenriquedb.todolist.task;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(unique = true, length = 50)
+    private String title;
+
+    private String description;
+
+    private Number priority;
+
+    private LocalDateTime startAt;
+
+    private LocalDateTime endAt;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    private UUID userId;
+}

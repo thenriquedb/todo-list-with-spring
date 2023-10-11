c43198e9758c0a16e3eae5370f2d6617f4ca306c
thiago <thenrique2012@gmail.com>
2023-10-11 18:09:48 -0300

    chore: rename user entity table

--- src/main/java/br/com/thenriquedb/todolist/user/UserModel.java
+++ src/main/java/br/com/thenriquedb/todolist/user/UserModel.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
-@Entity(name = "tb_user")
+@Entity(name = "tb_users")
 public class UserModel {
     @Id
     @GeneratedValue(generator = "UUID")

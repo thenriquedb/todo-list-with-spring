7f2ebd751e7f9a0a8bb1223e20be8af37ba9ff7e
thiago <thenrique2012@gmail.com>
2023-10-13 20:50:01 -0300

    feat: create exception handler

---
+++ src/main/java/br/com/thenriquedb/todolist/errors/ExceptionHandlerController.java
@@ -0,0 +1,19 @@
+package br.com.thenriquedb.todolist.errors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity handleHttpMessageNotReadableException(HttpMessageNotReadableException exception) {
+        String message = exception.getMostSpecificCause().getMessage()
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(message);
+    }
+}
--- src/main/java/br/com/thenriquedb/todolist/task/TaskModel.java
+++ src/main/java/br/com/thenriquedb/todolist/task/TaskModel.java
@@ -32,4 +32,12 @@ public class TaskModel {
     private LocalDateTime createdAt;
 
     private UUID userId;
+
+    public void setTitle(String title) throws Exception {
+        if(title.length() > 50) {
+            throw new Exception("The title field must be less than 50 characters");
+        }
+
+        this.title = title;
+    }
 }

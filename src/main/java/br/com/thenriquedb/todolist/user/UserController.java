26af405cd2f1eb2355ff092a8bfba277106205c7
thiago <thenrique2012@gmail.com>
2023-10-17 23:33:50 -0300

    docs: configure swagger for UserController

--- src/main/java/br/com/thenriquedb/todolist/user/UserController.java
+++ src/main/java/br/com/thenriquedb/todolist/user/UserController.java
@@ -1,23 +1,40 @@
 package br.com.thenriquedb.todolist.user;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
+@Tag(name = "User")
 public class UserController {
     @Autowired
     private IUserRepository userRepository;
 
-    @GetMapping
-    public  String get() {
-        return "Any user";
+    @GetMapping("/{id}")
+    @Operation(summary = "Get user from ID")
+    @ApiResponse(responseCode = "201")
+    public  ResponseEntity<Optional<UserModel>> get(@PathVariable UUID id) {
+        Optional<UserModel> user = this.userRepository.findById(id);
+        return ResponseEntity.ok(user);
     }
 
     @PostMapping
+    @Operation(summary = "Create new user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User created successfully"),
+            @ApiResponse(responseCode = "409", description = "Username already exists"),
+            @ApiResponse(responseCode = "500", description = "Internal error")
+    })
     public ResponseEntity create(@RequestBody UserModel userModel) {
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
